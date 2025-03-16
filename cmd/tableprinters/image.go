74a253c5dc1743f352aab36d436c377cfb1fe48a
Stefan Majer <stefan.majer@f-i-ts.de>
2025-03-16 09:54:44 +0100

    Image command

---
+++ cmd/tableprinters/image.go
@@ -0,0 +1,39 @@
+package tableprinters
+
+import (
+	"strings"
+
+	"github.com/metal-stack/api/go/enum"
+	apiv2 "github.com/metal-stack/api/go/metalstack/api/v2"
+	"github.com/metal-stack/metal-lib/pkg/pointer"
+	"github.com/olekukonko/tablewriter"
+)
+
+func (t *TablePrinter) ImageTable(data []*apiv2.Image, wide bool) ([]string, [][]string, error) {
+	var (
+		rows   [][]string
+		header = []string{"ID", "Name", "Description", "Features", "Expiration", "Status"}
+	)
+
+	for _, image := range data {
+		var (
+			features []string
+		)
+
+		for _, f := range image.Features {
+			feature, err := enum.GetStringValue(f)
+			if err != nil {
+				return nil, nil, err
+			}
+			features = append(features, feature)
+		}
+
+		rows = append(rows, []string{image.Id, pointer.SafeDeref(image.Name), pointer.SafeDeref(image.Description), strings.Join(features, ","), image.ExpiresAt.String(), image.Classification.String()})
+	}
+
+	t.t.MutateTable(func(table *tablewriter.Table) {
+		table.SetAutoWrapText(false)
+	})
+
+	return header, rows, nil
+}
