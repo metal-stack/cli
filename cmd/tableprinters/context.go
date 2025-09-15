5169d327b77ad79516ba4e088114fdea1a3c03e7
Gerrit <tirreg91@googlemail.com>
2025-09-15 13:09:28 +0200

    Found more stuff to carry over.

--- cmd/tableprinters/context.go
+++ cmd/tableprinters/context.go
@@ -3,6 +3,8 @@ package tableprinters
 import (
 	"github.com/fatih/color"
 	"github.com/metal-stack/cli/cmd/config"
+	"github.com/metal-stack/metal-lib/pkg/pointer"
+	"github.com/spf13/viper"
 )
 
 func (t *TablePrinter) ContextTable(data *config.Contexts, wide bool) ([]string, [][]string, error) {
@@ -11,12 +13,27 @@ func (t *TablePrinter) ContextTable(data *config.Contexts, wide bool) ([]string,
 		rows   [][]string
 	)
 
+	if wide {
+		header = append(header, "API URL")
+	}
+
 	for _, c := range data.Contexts {
 		active := ""
 		if c.Name == data.CurrentContext {
 			active = color.GreenString("✔")
 		}
-		rows = append(rows, []string{active, c.Name, c.Provider, c.DefaultProject})
+
+		row := []string{active, c.Name, c.Provider, c.DefaultProject}
+		if wide {
+			url := pointer.SafeDeref(c.ApiURL)
+			if url == "" {
+				url = viper.GetString("api-url")
+			}
+
+			row = append(row, url)
+		}
+
+		rows = append(rows, row)
 	}
 
 	return header, rows, nil
