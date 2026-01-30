56b7bbf4745e83911a8056691f5e1eb669a7d45e
Ilja Rotar <kontakt@ilja-rotar.de>
2026-01-30 15:07:45 +0100

    more sort keys for switch

--- cmd/sorters/switch.go
+++ cmd/sorters/switch.go
@@ -3,6 +3,7 @@ package sorters
 import (
 	apiv2 "github.com/metal-stack/api/go/metalstack/api/v2"
 	"github.com/metal-stack/metal-lib/pkg/multisort"
+	"github.com/metal-stack/metal-lib/pkg/pointer"
 )
 
 func SwitchSorter() *multisort.Sorter[*apiv2.Switch] {
@@ -13,6 +14,20 @@ func SwitchSorter() *multisort.Sorter[*apiv2.Switch] {
 		"description": func(a, b *apiv2.Switch, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.Description, b.Description, descending)
 		},
-		// TODO: also allow sorting by partition, rack, os, status, last sync time, replace mode, metal-core version, ip
+		"partition": func(a, b *apiv2.Switch, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.Partition, b.Partition, descending)
+		},
+		"rack": func(a, b *apiv2.Switch, descending bool) multisort.CompareResult {
+			return multisort.Compare(pointer.SafeDeref(a.Rack), pointer.SafeDeref(b.Rack), descending)
+		},
+		"os": func(a, b *apiv2.Switch, descending bool) multisort.CompareResult {
+			return multisort.Compare(pointer.SafeDeref(a.Os).Vendor, pointer.SafeDeref(b.Os).Vendor, descending)
+		},
+		"metal-core-version": func(a, b *apiv2.Switch, descending bool) multisort.CompareResult {
+			return multisort.Compare(pointer.SafeDeref(a.Os).MetalCoreVersion, pointer.SafeDeref(b.Os).MetalCoreVersion, descending)
+		},
+		"management-ip": func(a, b *apiv2.Switch, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.ManagementIp, b.ManagementIp, descending)
+		},
 	}, multisort.Keys{{ID: "id"}})
 }
