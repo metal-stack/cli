ed849d6f92d0e9f4d84d23565ca11e972a530a77
Stefan Majer <stefan.majer@f-i-ts.de>
2026-02-23 14:50:01 +0100

    Colored hints

--- cmd/tableprinters/component.go
+++ cmd/tableprinters/component.go
@@ -3,6 +3,7 @@ package tableprinters
 import (
 	"time"
 
+	"github.com/fatih/color"
 	"github.com/metal-stack/api/go/enum"
 	apiv2 "github.com/metal-stack/api/go/metalstack/api/v2"
 )
@@ -20,9 +21,21 @@ func (t *TablePrinter) ComponentTable(data []*apiv2.Component, wide bool) ([]str
 		}
 
 		started := humanizeDuration(time.Since(c.StartedAt.AsTime()))
-		age := humanizeDuration(time.Since(c.ReportedAt.AsTime()))
+		ageAsDuration := time.Since(c.ReportedAt.AsTime())
+		age := humanizeDuration(ageAsDuration)
 
-		tokenExpiresIn := humanizeDuration(time.Until(c.Token.Expires.AsTime()))
+		if c.Interval != nil && c.Interval.AsDuration() < ageAsDuration {
+			age = color.RedString(age)
+		}
+
+		tokenExpirationAsDuration := time.Until(c.Token.Expires.AsTime())
+		tokenExpiresIn := humanizeDuration(tokenExpirationAsDuration)
+
+		if tokenExpirationAsDuration < time.Hour {
+			tokenExpiresIn = color.YellowString(tokenExpiresIn)
+		} else if tokenExpirationAsDuration < 0 {
+			tokenExpiresIn = color.RedString(tokenExpiresIn)
+		}
 
 		rows = append(rows, []string{c.Uuid, *typeString, c.Identifier, started, age, c.Version.Version, c.Token.Uuid, tokenExpiresIn})
 	}
