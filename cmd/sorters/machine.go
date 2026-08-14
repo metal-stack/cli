b88d02a62a9388e56229cf0dad86380cab259825
Gerrit <tirreg91@googlemail.com>
2026-08-14 16:14:51 +0200

    Fix sorter nil pointers.

--- cmd/sorters/machine.go
+++ cmd/sorters/machine.go
@@ -9,16 +9,16 @@ import (
 func MachineSorter() *multisort.Sorter[*apiv2.Machine] {
 	return multisort.New(multisort.FieldMap[*apiv2.Machine]{
 		"partition": func(a, b *apiv2.Machine, descending bool) multisort.CompareResult {
-			return multisort.Compare(a.Partition.Id, b.Partition.Id, descending)
+			return multisort.Compare(pointer.SafeDeref(a.Partition).Id, pointer.SafeDeref(b.Partition).Id, descending)
 		},
 		"size": func(a, b *apiv2.Machine, descending bool) multisort.CompareResult {
-			return multisort.Compare(a.Size.Id, b.Size.Id, descending)
+			return multisort.Compare(pointer.SafeDeref(a.Size).Id, pointer.SafeDeref(b.Size).Id, descending)
 		},
 		"uuid": func(a, b *apiv2.Machine, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.Uuid, b.Uuid, descending)
 		},
 		"image": func(a, b *apiv2.Machine, descending bool) multisort.CompareResult {
-			return multisort.Compare(pointer.SafeDeref(a.Allocation).Image.Id, pointer.SafeDeref(b.Allocation).Image.Id, descending)
+			return multisort.Compare(pointer.SafeDeref(pointer.SafeDeref(a.Allocation).Image).Id, pointer.SafeDeref(pointer.SafeDeref(b.Allocation).Image).Id, descending)
 		},
 		"rack": func(a, b *apiv2.Machine, descending bool) multisort.CompareResult {
 			return multisort.Compare(a.Rack, b.Rack, descending)
@@ -27,7 +27,11 @@ func MachineSorter() *multisort.Sorter[*apiv2.Machine] {
 			return multisort.Compare(pointer.SafeDeref(a.Allocation).Project, pointer.SafeDeref(b.Allocation).Project, descending)
 		},
 		"age": func(a, b *apiv2.Machine, descending bool) multisort.CompareResult {
-			return multisort.Compare(pointer.SafeDeref(a.Allocation).Meta.CreatedAt.AsTime().Unix(), pointer.SafeDeref(b.Allocation).Meta.CreatedAt.AsTime().Unix(), descending)
+			return multisort.Compare(
+				new(pointer.SafeDeref(pointer.SafeDeref(pointer.SafeDeref(a.Allocation).Meta).CreatedAt)).AsTime().Unix(),
+				new(pointer.SafeDeref(pointer.SafeDeref(pointer.SafeDeref(b.Allocation).Meta).CreatedAt)).AsTime().Unix(),
+				descending,
+			)
 		},
 	}, multisort.Keys{{ID: "partition"}, {ID: "size"}, {ID: "project"}, {ID: "uuid"}})
 }
