144945dde8d2d181c97f1e0adfdf23985e000748
Stefan Majer <stefan.majer@f-i-ts.de>
2026-08-27 15:46:29 +0200

    Sort components (#56)

---
+++ cmd/sorters/component.go
@@ -0,0 +1,23 @@
+package sorters
+
+import (
+	apiv2 "github.com/metal-stack/api/go/metalstack/api/v2"
+	"github.com/metal-stack/metal-lib/pkg/multisort"
+)
+
+func ComponentSorter() *multisort.Sorter[*apiv2.Component] {
+	return multisort.New(multisort.FieldMap[*apiv2.Component]{
+		"type": func(a, b *apiv2.Component, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.Type, b.Type, descending)
+		},
+		"identifier": func(a, b *apiv2.Component, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.Identifier, b.Identifier, descending)
+		},
+		"started": func(a, b *apiv2.Component, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.StartedAt.AsTime().String(), b.StartedAt.AsTime().String(), descending)
+		},
+		"expiration": func(a, b *apiv2.Component, descending bool) multisort.CompareResult {
+			return multisort.Compare(a.Token.Expires.String(), b.Token.Expires.String(), descending)
+		},
+	}, multisort.Keys{{ID: "type", Descending: true}, {ID: "identifier", Descending: true}})
+}
